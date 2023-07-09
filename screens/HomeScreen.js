b93e377e3d800405c720a2619081d4b704abee9b
“esamsharf” <esamintech@gmail.com>
2023-07-09 21:34:37 +0200

    add my deliveroo app in React Native

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text
+        className="
+        text-4xl
+        text-center
+        text-red-500
+        bg-gray-200
+      "
+      >
+        HomeScreen
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
