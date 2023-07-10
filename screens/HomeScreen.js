456a56d4ccb2b4fb1fdfb7c87d97a2aa670aef2f
“esamsharf” <esamintech@gmail.com>
2023-07-10 11:34:34 +0200

    Update the HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,39 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import { Image, SafeAreaView, Text, View } from "react-native";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text
-        className="
-        text-4xl
-        text-center
-        text-red-500
-        bg-gray-200
-      "
-      >
-        HomeScreen
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-gray-400 text-lg">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={20} color="#00CCBB" />
+        </View>
       </Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
